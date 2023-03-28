c870ca90d9793045e6548b50e3d4c98de5d57f9a
LOVE <lovechen@me.com>
2023-03-28 13:12:42 +0800

    test

--- js/ua_mac_edge.js
+++ js/ua_mac_edge.js
@@ -1,17 +1,6 @@
 (function () {
-	const headers = $response.headers;
-	for (let key in headers) {
-		if (key.toUpperCase() === 'CONTENT-TYPE' && ! headers[key].includes('text/html')) {
-			$done({});
-			return;
-		}
-		if (key.toUpperCase() === 'CONTENT-SECURITY-POLICY') {
-			delete headers[key];
-		}
-	}
 	const body = $response.body.replace('<head>',
 	  '<head>' +
-	  '<script>delete navigator; window.navigator = { userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54" }</script>' +
-	  '<style>.ContentItem-actions>*:nth-child(3),.ContentItem-actions>*:nth-child(4){display:none}</style>');
-	$done({ headers, body })
+	  '<script>delete navigator; window.navigator = { userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54" }</script>');
+	$done({ body })
 })()
